27cf50e9ff78aed2c5b796aa487e7df422a6c0fd
akame <87499021+ckbi@users.noreply.github.com>
2023-08-17 08:49:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,6 @@
 
 					<textarea rows="12" class="form-control text-center form-checker mb-2" placeholder="PUT YOUR CARDS HERE"></textarea>
 					<textarea rows="1" class="form-control text-center" style="width: 70%; float: left ;"  id="sec" placeholder="sk_live_xxxxxx"></textarea>
-					<textarea rows="1"class="form-control text-center" style="width: 30%; float: right margin-bottom: 5px;" id="cst" placeholder="Custom Amount"></textarea></br>
 						<textarea rows="1"class="form-control text-center" style="width: 40%; float: left margin-bottom: 10px;" id="tgm" placeholder="Tg Id"></textarea></br>
 					       				          <select name="gate" id="gate" class="form-control" style="margin-bottom: 5px;"
 							  <option </option>
